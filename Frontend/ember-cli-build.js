b12ec7a51e642e5b624efe424697433aa72867d2
Dalton <dolphindalt@gmail.com>
2018-03-21 16:14:35 -0600

    Init SCSS

--- Frontend/ember-cli-build.js
+++ Frontend/ember-cli-build.js
@@ -4,10 +4,13 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
+    'sass-options': {
+      extension: 'scss'
+    },
     'ember-bootstrap': {
       'bootstrapVersion': 3,
       'importBootstrapFont': true,
-      'importBootstrapCSS': true
+      'importBootstrapCSS': false
     }
   });
 

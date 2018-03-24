989dd0b02194c77fec52444c393f4581d39ebab0
Dalton <dolphindalt@gmail.com>
2018-03-24 17:44:25 -0600

    Implementation of a user database handler with http protocols. Frontside handling of registering new users with error messages. SHA256 hashing for database passwords.

---
+++ Frontend/app/components/register-form.js
@@ -0,0 +1,55 @@
+import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import $ from 'jquery';
+import SHA256 from 'cryptojs/sha256';
+
+export default Component.extend({
+    currentDate: service('current-date'),
+    errorMessage: "",
+    router: service(),
+    actions: {
+        success(msg) {
+            this.get('router').transitionTo('sign-in');
+        },
+        register() {
+            let { username, passwd, passwd2} = this.getProperties('username', 'passwd', 'passwd2');
+
+            if(passwd != passwd2) {
+                this.set('errorMessage', "The two passwords entered did not match");
+                return;
+            }
+
+            if(username.length() < 6) {
+                this.set('errorMessage', "The username specified must be at least 6 characters");
+                return;
+            }
+
+            if(passwd.length() < 6) {
+                this.set('errorMessage', "The password must be at least 6 characters");
+                return;
+            }
+            
+            var hashword = SHA256(passwd).toString();
+            var comp = this; // stupid ajax
+            
+            $.ajax({
+                type: "POST",
+                url: "http://127.0.0.1:8000/register",
+                dataType: 'json',
+                contentType: "application/json; charset=utf-8",
+                crossDomain: true,
+                data: JSON.stringify({
+                    "username":username,
+                    "hashword":hashword,
+                    "date_created":this.get('currentDate').getDate()
+                }),
+                error: function(xhr) {
+                    comp.set('errorMessage', xhr.responseText);
+                },
+                success: function() {
+                    comp.send('success');
+                }
+            });
+        }
+    }
+});
--- Frontend/app/router.js
+++ Frontend/app/router.js
@@ -8,6 +8,7 @@ const Router = EmberRouter.extend({
  
 Router.map(function() {
   this.route('sign-in');
+  this.route('register');
 });
 
 export default Router;

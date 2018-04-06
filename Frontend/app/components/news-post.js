7ea3ccc0bce934724ecef905ac7d03104a2d2bcf
Dalton <dolphindalt@gmail.com>
2018-04-06 10:37:33 -0600

    A reminder

--- Frontend/app/components/news-post.js
+++ Frontend/app/components/news-post.js
@@ -1,5 +1,10 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-    classNames: [ "news-container" ]
+    classNames: [ "news-container" ],
+    actions: {
+        toPost() {
+            // todo: set up single post view model
+        }
+    }
 });
